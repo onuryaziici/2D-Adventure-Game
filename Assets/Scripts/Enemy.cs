a2a992cf83765a0bda4ceb10f6eb59e913b979c4
onuryaziici <82774284+onuryaziici@users.noreply.github.com>
2022-04-15 22:57:35 +0300

    Updated Melee Combat
    Updated melee combat and added enemy system

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Animator animator;
+    public int maxHealth = 100;
+    int currentHealth;
+    void Start()
+    {
+        currentHealth = maxHealth;
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -=damage;
+
+        //play hurt animation
+        animator.SetTrigger("Hurt");
+
+        if(currentHealth<=0)
+        {
+            Die();
+        }
+
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died !");
+        //die animation
+        animator.SetBool("IsDead",true);
+        //disable the enemy
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+
+    }
+
+}
--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -6,8 +6,9 @@ public class PlayerCombat : MonoBehaviour
 {
     public Animator animator;
     public Transform attackPoint;
-    public float attackRange=0.5f;
     public LayerMask enemyLayers;
+    public float attackRange=0.5f;
+    public int attackDamage = 40;
 
     // Update is called once per frame
     void Update()
@@ -24,7 +25,16 @@ public class PlayerCombat : MonoBehaviour
         Collider2D[]  hitEnemies= Physics2D.OverlapCircleAll(attackPoint.position,attackRange,enemyLayers);
         foreach(Collider2D enemy in hitEnemies)
         {
-            Debug.Log("We hit"+enemy.name);
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
         }
+    }
+
+    void OnDrawGizmosSelected() 
+    {
+        if (attackPoint == null)
+        return;
+
+        Gizmos.DrawWireSphere(attackPoint.position,attackRange);
+
     }
 }
